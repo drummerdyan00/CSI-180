aac5c6a736ac9448df8000bc4f6baf61d094acdf
drummerdyan00 <62715699+drummerdyan00@users.noreply.github.com>
2023-05-04 17:53:54 -0400

    Folders of various code I've used and look aat

---
+++ colorwheel_32x32/colorwheel_32x32.ino
@@ -0,0 +1,47 @@
+// colorwheel demo for Adafruit RGBmatrixPanel library.
+// Renders a nice circle of hues on our 32x32 RGB LED matrix:
+// http://www.adafruit.com/products/607
+// 32x32 MATRICES DO NOT WORK WITH ARDUINO UNO or METRO 328.
+
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <RGBmatrixPanel.h>
+#include <string> 
+// Most of the signal pins are configurable, but the CLK pin has some
+// special constraints.  On 8-bit AVR boards it must be on PORTB...
+// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
+// on the same PORT as the RGB data pins (D2-D7)...
+// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
+// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
+// Matrix Shield, cut trace between CLK pads and run a wire to A4).
+
+#define CLK  8   // USE THIS ON ADAFRUIT METRO M0, etc.
+//#define CLK A4 // USE THIS ON METRO M4 (not M0)
+#define CLK 11 // USE THIS ON ARDUINO MEGA
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+int game_time=7,200,000; 
+int _time
+
+bool delayRunning = false
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+
+void setup() {
+}
+void loop() {
+if (delayRunning && ((millis() - delayStart) >= 1000)) {
+    delayRunning = false; // // prevent this code being run more then once
+    ; // This second change to update timer code
+    Serial.println("Updating ");
+}
+display.print(time_left);
+  display.display();
+delay(1000)
+}
